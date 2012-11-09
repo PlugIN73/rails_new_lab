0fb1c9f37a2d4fd839c111d62bc6d8f95d858dd9
plugin73 <plugin73@plugin73-ubuntu.(none)>
2012-11-09 21:52:11 +0400

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RailsNewLab::Application.config.secret_token = '0ac5090b214ef198ce97e92b5daf55d84da4e3c60ddedf6fce5d5442a006c5a27ed1c56d3073b9c339eaab3138491bfa1c376f69e18a0a279b6e45e0bd614583'
---
+++ db/migrate/20121109174650_create_posts.rb
@@ -0,0 +1,12 @@
+class CreatePosts < ActiveRecord::Migration
+  def change
+    create_table :posts do |t|
+      t.datetime :published_at
+      t.string :title
+      t.string :description
+      t.text :body
+
+      t.timestamps
+    end
+  end
+end
